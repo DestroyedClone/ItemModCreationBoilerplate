c2bfcdeceb044e742064ad618922ecfdd443e62a
DestroyedClone <destroyedclone@gmail.com>
2023-05-27 14:02:54 -0700

    Added missing LanguageOverrides call Updated Items.language and ExampleItem to demonstrate the ReplacementTokens

--- ItemModCreationBoilerplate/Items/ExampleItem.cs
+++ ItemModCreationBoilerplate/Items/ExampleItem.cs
@@ -1,6 +1,8 @@
 ﻿using BepInEx.Configuration;
+using ItemModCreationBoilerplate.Utils;
 using R2API;
 using RoR2;
+using System;
 using UnityEngine;
 using static ItemModCreationBoilerplate.Main;
 
@@ -11,6 +13,16 @@ namespace ItemModCreationBoilerplate.Items
         public override string ItemName => "Deprecate Me Item";
 
         public override string ItemLangTokenName => "DEPRECATE_ME_ITEM";
+
+        public override string[] ItemFullDescriptionParams => new string[]
+        {
+            (0.5f*100).ToString(),
+            100.ToString()
+        };
+        public override string[] ItemPickupDescParams => new string[]
+        {
+            112345f.ToString()
+        };
 
         public override ItemTier Tier => ItemTier.Tier1;
 
--- ItemModCreationBoilerplate/Main.cs
+++ ItemModCreationBoilerplate/Main.cs
@@ -43,6 +43,7 @@ namespace ItemModCreationBoilerplate
             // Look here for info on how to set these up: https://github.com/KomradeSpectre/AetheriumMod/blob/rewrite-master/Tutorials/Item%20Mod%20Creation.md#unity-project
             // (This is a bit old now, but the information on setting the unity asset bundle should be the same.)
             Assets.Init();
+            Modules.LanguageOverrides.Initialize();
 
             //This section automatically scans the project for all artifacts
             var ArtifactTypes = Assembly.GetExecutingAssembly().GetTypes().Where(type => !type.IsAbstract && type.IsSubclassOf(typeof(ArtifactBase)));
